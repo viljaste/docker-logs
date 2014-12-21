41141c72de4026afad3eaf05e5d61b5bcbe09486
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-21 13:40:41 +0200

    Update

--- src/build/modules/logs/manifests/fluentd.pp
+++ src/build/modules/logs/manifests/fluentd.pp
@@ -2,9 +2,8 @@ class logs::fluentd {
   require logs::fluentd::packages
   require logs::fluentd::supervisor
 
-  exec { 'td-agent-gem install fluent-plugin-elasticsearch':
-    timeout => 0,
-    path => ['/usr/sbin']
+  exec { '/bin/su - root -c "td-agent-gem install fluent-plugin-elasticsearch"':
+    timeout => 0
   }
 
   file { '/root/fluentd.conf.tmpl':
